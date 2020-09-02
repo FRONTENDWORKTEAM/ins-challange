59dd3da08565f82b0a2539c4d986a31bb54e15e2
Erdoğan BULUT <e-bulut@hotmail.com.tr>
2020-09-02 17:59:57 +0300

    Create header component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
   #app
     AppIconset
+    AppHeader
     router-view
 </template>
 
 <script>
 import AppIconset from './components/layout/AppIconset.vue';
+import AppHeader from './components/layout/AppHeader.vue';
 
 export default {
   components: {
     AppIconset,
+    AppHeader,
   },
 };
 </script>
---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  header
+    AppNavbar
+    AppBreadcrumb
+</template>
+
+<script>
+import AppNavbar from './AppNavbar.vue';
+import AppBreadcrumb from './AppBreadcrumb.vue';
+
+export default {
+  components: {
+    AppNavbar,
+    AppBreadcrumb,
+  },
+};
+</script>
