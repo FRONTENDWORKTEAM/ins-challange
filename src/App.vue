01e986b5442d4b081e96bcc8ff5ec25de48070df
Erdoğan BULUT <e-bulut@hotmail.com.tr>
2020-09-02 16:59:01 +0300

    Register iconset to app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template lang="pug">
   #app
+    AppIconset
     router-view
 </template>
+
+<script>
+import AppIconset from './components/layout/AppIconset.vue';
+
+export default {
+  components: {
+    AppIconset,
+  },
+};
+</script>
 
 <style lang="scss">
 @import './styles/vendors.scss';
