f912c00d4c72b96bc685ffcc450b51789559ccf5
Erdoğan BULUT <e-bulut@hotmail.com.tr>
2020-09-02 18:55:50 +0300

    Create AppHeaderTitle component

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -2,16 +2,19 @@
   header
     AppNavbar
     AppBreadcrumb
+    AppHeaderTitle
 </template>
 
 <script>
 import AppNavbar from './AppNavbar.vue';
 import AppBreadcrumb from './AppBreadcrumb.vue';
+import AppHeaderTitle from './AppHeaderTitle.vue';
 
 export default {
   components: {
     AppNavbar,
     AppBreadcrumb,
+    AppHeaderTitle,
   },
 };
 </script>
---
+++ src/components/layout/AppHeaderTitle.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  section.component.is-header-title
+    .container-fluid
+      .header-title--columns
+        .header-title--columns__item.is-title
+          h3 Whom to Target?
+          p Select one or more user groups to show your personalization.
+        .header-title--columns__item.is-controls
+          .header-title--control
+            label.header-title--control__label.form-label(for="inputHeaderTitleName")
+              | Personalization Name
+            input.header-title--control__input#inputHeaderTitleName.form-control(
+              type="text"
+              value="Maven-Test")
+</template>
+
+<style lang="scss" scoped>
+@import '../../styles/shareds.scss';
+
+$this: 'header-title';
+
+section.component {
+  &.is-#{$this} {
+    border-bottom: 1px solid $colorBorder;
+    .#{$this} {
+      &--columns {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        &__item {
+          padding: 1.5rem 1rem;
+          &.is-title {
+            flex-basis: 0;
+            flex-grow: 1;
+          }
+          &.is-controls {
+            width: 20rem;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
