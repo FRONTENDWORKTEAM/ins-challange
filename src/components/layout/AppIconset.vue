46194552f9b07edbd2728aade81d7605e457070d
Erdoğan BULUT <e-bulut@hotmail.com.tr>
2020-09-02 14:54:12 +0300

    Create Icon and Iconset component with some icons

---
+++ src/components/layout/AppIconset.vue
@@ -0,0 +1,26 @@
+<script>
+/* eslint-disable max-len */
+
+/**
+ * !!! README
+ * !!! IMPORTANT
+ * !!! THE VİEWBOX VALUES OF THE ADDED SVGS (SYMBOLS) MUST BE DELETED AND ADDED TO shareds/Icon.vue.
+ */
+export default {
+  name: 'Iconset',
+};
+</script>
+
+<template>
+  <svg class="defs-only" xmlns="http://www.w3.org/2000/svg">
+    <symbol id="icon-search" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M8 16c1.805 0 3.47-.598 4.808-1.606l3.278 3.278a1.121 1.121 0 001.586-1.586l-3.278-3.278A8 8 0 108 16zm0-2a5.972 5.972 0 003.375-1.039 6.034 6.034 0 001.586-1.586A6 6 0 108 14z"/></symbol>
+    <symbol id="icon-arrow-left" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M5.3.197a.658.658 0 01.94 0 .663.663 0 010 .93l-3.21 3.21h11.313c.366 0 .657.292.657.658a.662.662 0 01-.658.667H3.03l3.21 3.202a.674.674 0 010 .939.658.658 0 01-.94 0L.964 5.465a.647.647 0 010-.93L5.3.197z"/></symbol>
+    <symbol id="icon-chevron-right" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M.306 9.694a1.045 1.045 0 010-1.478L3.522 5 .306 1.784A1.045 1.045 0 011.784.306l3.91 3.91c.215.216.317.502.305.784.012.282-.09.568-.305.784l-3.91 3.91a1.045 1.045 0 01-1.478 0z"/></symbol>
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+  svg {
+    display: none;
+  }
+</style>
---
+++ src/components/shareds/Icon.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  svg(:viewBox="getViewBox()")
+    use(:xlink:href="`#icon-${name}`")
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  data() {
+    return {
+      icons: [
+        {
+          name: 'search',
+          viewBox: '0 0 18 18',
+        },
+        {
+          name: 'arrow-left',
+          viewBox: '0 0 15 10',
+        },
+        {
+          name: 'chevron-right',
+          viewBox: '0 0 6 10',
+        },
+      ],
+    };
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    getViewBox() {
+      return this.icons.find((i) => i.name === this.name).viewBox;
+    },
+  },
+};
+</script>
